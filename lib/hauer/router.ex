c9ae7887181ebfa59ae2f114627e555859ede751
Federico Caprari <korsmakolnikov@gmail.com>
2019-09-01 23:40:11 +0200

    Improve proposal, but still not working. Also I've investigated on the problem. Refer to comments. Drop it when you want.

--- lib/hauer/router.ex
+++ lib/hauer/router.ex
@@ -8,15 +8,21 @@ defmodule Hauer.Router do
 
     parsed = YamerlWrapper.file(@conf_file)
 
+    #TODO what follow can't work because define_ruote cannot be executed outside a function. 
+    #TODO actual if we want to see this works we have to switch to macros because you cannot
+    #TODO invoke "get/1" macro inside another macro-like as start_link. 
+
     # [[{'resources', ['example1', 'example2']}]]
     [[{_, resources}]] = parsed
 
-    # TODO make it work! :D
-    Enum.map(resources, fn resource ->
-        get "/#{resource}" do
-            conn |> send_resp(200, resource)
+    def define_route r do
+        get "/#{r}" do
+            conn |> send_resp(200, r)
         end
-    end)
+    end
+
+    resources |>
+        Enum.map(&define_route/1)
 
     def start_link do
         Plug.Adapters.Cowboy2.http(__MODULE__, [])
