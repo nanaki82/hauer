52f30d221161558c3b012cd41ad0ae005e2e5c83
Federico Caprari <korsmakolnikov@gmail.com>
2019-10-08 21:52:06 +0200

    Update Hauer.Configuration to use Jason

--- lib/hauer/configuration.ex
+++ lib/hauer/configuration.ex
@@ -1,7 +1,37 @@
 defmodule Hauer.Configuration do
   @conf_file Application.get_env(:hauer, :conf_file)
 
-  def read do
-    YamerlWrapper.file(@conf_file)
+  def read do 
+    File.open! @conf_file, [:read, :utf8], fn file -> 
+      conf = IO.read file, :all
+      Jason.decode!(conf, %{:keys => :atoms})
+    end
+  end
+
+  def write new_conf do
+    encoded_conf = Jason.encode!(new_conf, %{:pretty => true})
+    {:ok, pwd} = File.cwd()
+
+    conf_file_path = "#{pwd}/#{@conf_file}"
+
+    # todo remove it
+    conf_file_exists? = File.exists?(conf_file_path)
+
+    if conf_file_exists? == false do
+      File.touch!(conf_file_path)
+    end
+
+    File.write! conf_file_path, encoded_conf, [:write, :utf8]
+  end
+
+
+  def parse_conf nil do
+    []
+  end
+
+  def parse_conf resources do
+    resources
+      |> Enum.map(fn x -> Map.to_list(x) |> List.first end) 
+      |> Enum.map(fn ({resource, _}) -> resource end)
   end
 end
