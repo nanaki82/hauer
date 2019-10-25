b0c9163a5bca96e2062a96664adfb489a6a7fcff
Carlo Tomei <carlo.tomei@adespresso.com>
2019-10-25 13:42:44 +0200

    Refactor mix task to create a resurce

--- lib/mix/tasks/hauer.ex
+++ lib/mix/tasks/hauer.ex
@@ -8,59 +8,24 @@ defmodule Mix.Tasks.Hauer do
   @impl Mix.Task
   @shortdoc "Scaffold a new route"
   def run(args) do
-    IO.puts("Hauer command line 1.0")
-    # todo remove, debug purpose only
-    args_string = Enum.join(args, ", ")
-    Mix.shell().info(args_string)
+    Mix.shell().info("Hauer command line 1.0")
 
     verb = args |> Enum.at(0)
     resource_name = args |> Enum.at(1)
 
     case verb do
-      "update" ->
-        Mix.shell().info("Updating configuration with #{resource_name}")
-        update_config(resource_name)
-        :ok
-
       "resource" ->
-        Mix.shell().info("Creating #{resource_name}")
-        new_resource(resource_name)
-        :ok
-
+        case Hauer.FS.add_resource(resource_name) do
+          {:ok, _} -> 
+            Hauer.Configuration.add_resource(resource_name)
+            Mix.shell().info("Created #{resource_name}")
+          {:error, message} -> 
+            Mix.shell().error(message)
+        end
       _ ->
         Mix.shell().info("I'm afraid I can't do that, Dave.")
     end
 
     Mix.shell().info("All done.")
-  end
-
-  @doc """
-  Create a new resource tree in the lib directory. Module and directory will
-  have the same name of the resource.
-  """
-  def new_resource(resource_name) do
-    {:ok, pwd} = File.cwd()
-    full_path = "#{pwd}/lib/#{resource_name}"
-    module_full_path = "#{full_path}/#{resource_name}.ex"
-
-    # todo remove it
-    Mix.shell().info(full_path)
-    it_s_new? = !File.exists?(module_full_path)
-
-    case it_s_new? do
-      true ->
-        File.mkdir!(full_path)
-        File.touch!(module_full_path)
-
-      _ ->
-        Mix.shell().info("the resource allready exists in #{module_full_path}")
-    end
-  end
-
-  @doc """
-  Update the project configuration according to resource name
-  """
-  def update_config(resource_name) do
-    Hauer.Configuration.add_resource(resource_name)
   end
 end
